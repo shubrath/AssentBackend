3c1b45ca9e7edc35e2a39f91d569c7a1315c59f9
shubrath <shubrathkg@gmail.com>
2024-12-22 11:45:46 +0530

    added

---
+++ AssentProject/src/main/java/com/Assent/AssentProject/AssentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Assent.AssentProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssentProjectApplication.class, args);
+	}
+
+}
